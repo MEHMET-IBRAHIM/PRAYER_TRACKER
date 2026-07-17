be69adf61f765ca6917739d8c49f92429f107d40
MEHMET IBRAHIM <stlaptop10@gmail.com>
2026-07-18 02:31:24 +0500

    DAILY PRAYER STATS

---
+++ daily_prayer_tracker.py
@@ -0,0 +1,13 @@
+
+
+
+print("ASSALAM-U-ALAIKUM ! AKHI")
+print("WHAT is your name?")
+y_name=input()
+print("MAY ALLAH BLESS YOU," , y_name ,"How many prayers do u pray today? NAME THEM---")
+prayers=list(input())
+print(y_name," prayed" ,  prayers , "today")
+if len(prayers)<5:
+    print("---MAY ALLAH BLESS YOU----\n ---DONT MISS PRAYERS AKHI---\n ** THE HOLY QURAN SAID:\n\n ---Translation:Indeed, prayer prohibits immorality and wrongdoing.----\n (Quran 29:45)")
+else:
+    print("---MAY ALLAH BLESS YOU-----\n ---DONT MISS PRAYER---\n----MAY ALLAH GUIDE AND HELP AND PROTECT YOU")
